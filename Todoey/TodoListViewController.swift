ce37d235fa290fd473a52017bda5513f5d8ff43e
Seungil Baek <sibaek31@gmail.com>
2022-04-13 13:11:31 +0900

    TableView Delegate Methods and Accessories

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,6 +17,8 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK - Tableview Datasource Methods
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -26,6 +28,20 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    //MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
 
 
 }
